c5bec43ddc1561b5bdc054ebdabce2c4f2a3e118
lorikklenja <lorikklenja@gmail.com>
2023-02-16 16:39:03 +0100

    fixes

--- resources/views/jobs/applications.blade.php
+++ resources/views/jobs/applications.blade.php
@@ -1,4 +1,11 @@
 <x-app-layout>
+
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.4/js/buttons.html5.min.js">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.4/js/buttons.print.min.js"> --}}
+
     <script>
         $(document).ready(function() {
     $('#mytable').DataTable();
